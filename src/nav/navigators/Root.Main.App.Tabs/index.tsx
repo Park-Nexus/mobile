070e53732da616e006fa6507adbd0a27200e8574
vclong2003 <vclong2003@gmail.com>
2024-09-28 23:18:45 +0700

    refactor: Update navigation stack in Root.Main.App and Root.Main.Auth
    - Refactor the navigation stack in Root.Main.App and Root.Main.Auth
    - Update the import statement for the Onboarding screen to Test in Root.Main.App
    - Rename the Onboarding component to Test in Root.Main.App/Test

---
+++ src/nav/navigators/Root.Main.App.Tabs/index.tsx
@@ -0,0 +1,5 @@
+import {createBottomTabNavigator} from '@src/libs/bottomTabNavigator';
+
+const {Navigator, Screen} = createBottomTabNavigator();
+
+export function TabNavigator() {}
--- src/nav/navigators/Root.Main.App/index.tsx
+++ src/nav/navigators/Root.Main.App/index.tsx
@@ -1,12 +1,12 @@
 import {createStackNavigator} from '@src/libs/stackNavigator';
-import {Onboarding} from '@src/nav/screens/Root.Main.App/Onboarding';
+import {Test} from '@src/nav/screens/Root.Main.App/Test';
 
 const {Navigator, Screen} = createStackNavigator();
 
 export function AppNavigator() {
   return (
     <Navigator>
-      <Screen name="Onboarding" component={Onboarding} />
+      <Screen name="Test" component={Test} />
     </Navigator>
   );
 }
