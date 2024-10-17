ea2fac86b3e58e4f0d32c190909e23ef99d6213b
vclong2003 <vclong2003@gmail.com>
2024-10-17 16:50:34 +0700

    Parking lot detail popup

--- src/nav/screens/Root.Main.App.Tabs/Home/Home.LotDetailModal/index.styles.ts
+++ src/nav/screens/Root.Main.App.Tabs/Home/Home.LotDetailModal/index.styles.ts
@@ -1,38 +1,68 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  wrapper: {
-    height: 220,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 12,
-    padding: 20,
-    paddingTop: 0,
-  },
+    wrapper: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 12,
+        padding: 20,
+        paddingTop: 0,
+    },
 
-  pill: {
-    backgroundColor: '#d7d7d7',
-    width: 50,
-    height: 4,
-    borderRadius: 8,
-    alignSelf: 'center',
-    marginVertical: 8,
-  },
+    pill: {
+        backgroundColor: '#d7d7d7',
+        width: 50,
+        height: 4,
+        borderRadius: 8,
+        alignSelf: 'center',
+        marginVertical: 8,
+    },
+    titleWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    titleItemWrapper: {
+        flexDirection: 'column',
+        gap: 4,
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: '700',
+    },
+    subTitleText: {
+        color: '#5A5555',
+        fontSize: 14,
+        fontWeight: '400',
+        maxWidth: 180,
+    },
 
-  titleWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  titleItemWrapper: {
-    flexDirection: 'column',
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  subTitleText: {
-    color: '#5A5555',
-    fontSize: 14,
-    fontWeight: '400',
-    maxWidth: 180,
-  },
+    summaryWrapper: {
+        flexDirection: 'row',
+        gap: 24,
+        marginVertical: 16,
+    },
+    summaryItemWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
+    },
+    summaryText: {
+        fontSize: 14,
+        fontWeight: '600',
+        color: '#5A5555',
+    },
+
+    detailWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 16,
+    },
+    servicesWrapper: {
+        flexDirection: 'row',
+        gap: 8,
+    },
+    detailButton: {
+        paddingHorizontal: 0,
+        marginHorizontal: 0,
+    },
 });
--- src/nav/screens/Root.Main.App.Tabs/Home/Home.LotDetailModal/index.tsx
+++ src/nav/screens/Root.Main.App.Tabs/Home/Home.LotDetailModal/index.tsx
@@ -8,48 +8,87 @@ import {useParkingLot} from './index.$data';
 import {reverseGeocode} from '@src/utils/location';
 import {useEffect, useState} from 'react';
 
+import MapPinSvg from '@src/static/svgs/MapPinArea.svg';
+import LetterPSvg from '@src/static/svgs/LetterCircleP.svg';
+import CaretRightSvg from '@src/static/svgs/CaretRight.svg';
+import {LOT_SERVICE_ICONS} from './index.types';
+
 export function LotDetailModal() {
-  const {selectedLotId, setSelectedLotId} = useHomeContext();
-  const {bottom} = useSafeAreaInsets();
-  const {data: lot, isFetching} = useParkingLot();
+    const {selectedLotId, setSelectedLotId} = useHomeContext();
+    const {bottom} = useSafeAreaInsets();
+    const {data: lot, isFetching} = useParkingLot();
 
-  const [address, setAddress] = useState<string>('');
-  const prices = lot?.parkingLotPrices || [];
-  const maxPrice = prices.reduce((acc, price) => Math.max(acc, price.price), 0);
-  const minPrice = prices.reduce((acc, price) => Math.min(acc, price.price), maxPrice);
+    const [address, setAddress] = useState<string>('');
 
-  useEffect(() => {
-    reverseGeocode({
-      lat: lot?.latitude,
-      lon: lot?.longitude,
-    }).then(setAddress);
-  }, [lot]);
+    const prices = lot?.parkingLotPrices || [];
+    const maxPrice = prices.reduce((acc, price) => Math.max(acc, price.price), 0);
+    const minPrice = prices.reduce((acc, price) => Math.min(acc, price.price), maxPrice);
 
-  return (
-    <Modal
-      animationIn="slideInUp"
-      animationInTiming={400}
-      animationOut={'slideOutDown'}
-      animationOutTiming={700}
-      style={{justifyContent: 'flex-end', paddingBottom: bottom + 70}}
-      isVisible={!!selectedLotId}
-      hasBackdrop={false}
-      swipeDirection="down"
-      onSwipeComplete={() => setSelectedLotId(undefined)}
-      coverScreen={false}>
-      <View style={styles.wrapper}>
-        <View style={styles.pill} />
-        <View style={styles.titleWrapper}>
-          <View style={styles.titleItemWrapper}>
-            <Text style={styles.titleText}>{lot?.name}</Text>
-            <Text style={styles.subTitleText}>{address}</Text>
-          </View>
-          <View style={styles.titleItemWrapper}>
-            <Text style={styles.titleText}>${minPrice}</Text>
-            <Text style={styles.subTitleText}>per hour</Text>
-          </View>
-        </View>
-      </View>
-    </Modal>
-  );
+    const spotsAvailable = lot?.parkingSpots?.reduce((acc, spot) => {
+        if (spot.isAvailable) return acc + 1;
+        return acc;
+    }, 0);
+
+    useEffect(() => {
+        reverseGeocode({
+            lat: lot?.latitude,
+            lon: lot?.longitude,
+        }).then(setAddress);
+    }, [lot]);
+
+    return (
+        <Modal
+            animationIn="slideInUp"
+            animationInTiming={400}
+            animationOut={'slideOutDown'}
+            animationOutTiming={700}
+            style={{justifyContent: 'flex-end', paddingBottom: bottom + 70}}
+            isVisible={!!selectedLotId}
+            hasBackdrop={false}
+            swipeDirection="down"
+            onSwipeComplete={() => setSelectedLotId(undefined)}
+            coverScreen={false}>
+            <View style={styles.wrapper}>
+                <View style={styles.pill} />
+
+                <View style={styles.titleWrapper}>
+                    <View style={styles.titleItemWrapper}>
+                        <Text style={styles.titleText}>{lot?.name}</Text>
+                        <Text style={styles.subTitleText}>{address}</Text>
+                    </View>
+                    <View style={styles.titleItemWrapper}>
+                        <Text style={[styles.titleText, {textAlign: 'right'}]}>${minPrice}</Text>
+                        <Text style={styles.subTitleText}>per hour</Text>
+                    </View>
+                </View>
+
+                <View style={styles.summaryWrapper}>
+                    <View style={styles.summaryItemWrapper}>
+                        <MapPinSvg width={24} height={24} />
+                        <Text style={styles.summaryText}>720m</Text>
+                    </View>
+                    <View style={styles.summaryItemWrapper}>
+                        <LetterPSvg width={24} height={24} />
+                        <Text style={styles.summaryText}>{spotsAvailable} slots</Text>
+                    </View>
+                </View>
+
+                <View style={styles.detailWrapper}>
+                    <View style={styles.servicesWrapper}>
+                        {lot?.parkingLotServices?.map((service, index) => (
+                            <View key={index}>{LOT_SERVICE_ICONS[service.type]}</View>
+                        ))}
+                    </View>
+                    <Button
+                        variant="white"
+                        text="Details"
+                        postIcon={<CaretRightSvg width={24} height={24} />}
+                        style={styles.detailButton}
+                    />
+                </View>
+
+                <Button variant="green" text="Book now" />
+            </View>
+        </Modal>
+    );
 }
