fb954cb43d09e56e7ba7dafb65644eb289a4a0e2
vclong2003 <vclong2003@gmail.com>
2024-10-23 22:49:48 +0700

    ok

--- src/nav/screens/Root.Main.App/ParkingLot__Detail/index.styles.ts
+++ src/nav/screens/Root.Main.App/ParkingLot__Detail/index.styles.ts
@@ -5,11 +5,29 @@ export const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 20,
         paddingTop: 16,
+        gap: 32,
     },
 
     image: {
         width: '100%',
         height: 200,
         borderRadius: 16,
+    },
+
+    infoWrapper: {
+        gap: 16,
+    },
+    textWrapper: {
+        gap: 4,
+    },
+    nameText: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#000',
+    },
+    addressText: {
+        fontSize: 14,
+        fontWeight: '400',
+        color: '#5A5555',
     },
 });
--- src/nav/screens/Root.Main.App/ParkingLot__Detail/index.tsx
+++ src/nav/screens/Root.Main.App/ParkingLot__Detail/index.tsx
@@ -1,4 +1,4 @@
-import {NavigatorScreenParams, RouteProp} from '@react-navigation/native';
+import {RouteProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {Header} from '@src/components/Header';
 import {SafeAreaView} from '@src/components/SafeAreaWrapper';
@@ -7,6 +7,8 @@ import {Image, Text, View} from 'react-native';
 import {styles} from './index.styles';
 import {useParkingLot} from './index.data';
 import FastImage from 'react-native-fast-image';
+import {useEffect, useState} from 'react';
+import {reverseGeocode} from '@src/utils/location';
 
 type ParkingLotDetailProps = {
     navigation: NativeStackNavigationProp<RootStackParamList, 'ParkingLot__Detail'>;
@@ -17,6 +19,17 @@ export function ParkingLotDetail({route, navigation}: ParkingLotDetailProps) {
     const {lotId} = route.params;
     const {data: lot} = useParkingLot(lotId);
 
+    const [address, setAddress] = useState<string>('');
+
+    useEffect(() => {
+        if (lot) {
+            reverseGeocode({
+                lat: lot.latitude,
+                lon: lot.longitude,
+            }).then(setAddress);
+        }
+    }, [lot]);
+
     return (
         <SafeAreaView>
             <Header backButtonVisible title="Parking Lot Details" onBackButtonPress={() => navigation.goBack()} />
@@ -24,6 +37,12 @@ export function ParkingLotDetail({route, navigation}: ParkingLotDetailProps) {
                 {lot?.mediaUrls?.length && lot?.mediaUrls?.length > 0 && (
                     <FastImage source={{uri: lot?.mediaUrls[0]}} style={styles.image} resizeMode="cover" />
                 )}
+                <View style={styles.infoWrapper}>
+                    <View style={styles.textWrapper}>
+                        <Text style={styles.nameText}>{lot?.name}</Text>
+                        <Text style={styles.addressText}>{address}</Text>
+                    </View>
+                </View>
                 <Text>
                     {lot?.name} {lot?.mediaUrls}
                 </Text>
