7edf48ea1ac0fa28870a1912f1078852a2ed12bb
vclong2003 <vclong2003@gmail.com>
2024-11-03 01:38:21 +0700

    ok

--- src/nav/screens/Root.Main.App.Tabs/LotManagement/index.tsx
+++ src/nav/screens/Root.Main.App.Tabs/LotManagement/index.tsx
@@ -1,5 +1,15 @@
-import {Text} from 'react-native';
+import {Header} from '@src/components/Header';
+import {SafeAreaView} from '@src/components/SafeAreaWrapper';
+import {Text, View} from 'react-native';
+import {styles} from './index.styles';
+
+import PlusTealSvg from '@src/static/svgs/Plus.svg';
 
 export function LotManagement() {
-    return <Text>Lot Management</Text>;
+    return (
+        <SafeAreaView>
+            <Header title="Lot Management" rightButtonIcon={<PlusTealSvg width={24} height={24} />} />
+            <View style={styles.wrapper}></View>
+        </SafeAreaView>
+    );
 }
